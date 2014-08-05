aebd8c2c6542f515c401fe46656dfdc4aeddce91
Saysa <saysabou@gmail.com>
2014-08-05 23:43:00 +0200

    Can get data from HTTP POST

--- src/Vyper/SiteBundle/Controller/UsersController.php
+++ src/Vyper/SiteBundle/Controller/UsersController.php
@@ -4,15 +4,25 @@ namespace Vyper\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Vyper\SiteBundle\Form\Login\Login;
+use Symfony\Component\HttpFoundation\Request;
 
 class UsersController extends Controller
 {
     /**
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
         $form = $this->get('form.factory')->create(new Login());
+        if ('POST' === $request->getMethod()) {
+
+            $form->handleRequest($request);
+            $data = $form->getData();
+            var_dump($data);
+        }
+
+
 
         return $this->render('VyperSiteBundle:Users:login.html.twig', array(
             'form' => $form->createView()
