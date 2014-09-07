56ee18ecb31f099277d358061495f4b85445de26
saysa <saysabou@gmail.com>
2014-09-07 16:14:49 +0200

    Fixtures Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,11 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            #new Vyper\SiteBundle\VyperSiteBundle(),
+            new Vyper\SiteBundle\VyperSiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Vyper\SiteBundle\VyperSiteBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
