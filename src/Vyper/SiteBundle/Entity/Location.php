5ce664c4f41977cc69114441dc45e010bf28cd2a
Saysa Bounkhong <saysa@Saysa-3.local>
2014-09-08 15:51:55 +0200

    Creation Entity Location

---
+++ src/Vyper/SiteBundle/Entity/Location.php
@@ -0,0 +1,394 @@
+<?php
+
+namespace Vyper\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Location
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Vyper\SiteBundle\Entity\LocationRepository")
+ */
+class Location
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nameReal", type="string", length=255)
+     */
+    private $nameReal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="town", type="string", length=255)
+     */
+    private $town;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="townReal", type="string", length=255)
+     */
+    private $townReal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="text")
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="access", type="text")
+     */
+    private $access;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="googlemap", type="text")
+     */
+    private $googlemap;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="live", type="boolean")
+     */
+    private $live;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="deleted", type="boolean")
+     */
+    private $deleted;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified", type="datetime")
+     */
+    private $modified;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Location
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nameReal
+     *
+     * @param string $nameReal
+     * @return Location
+     */
+    public function setNameReal($nameReal)
+    {
+        $this->nameReal = $nameReal;
+
+        return $this;
+    }
+
+    /**
+     * Get nameReal
+     *
+     * @return string 
+     */
+    public function getNameReal()
+    {
+        return $this->nameReal;
+    }
+
+    /**
+     * Set town
+     *
+     * @param string $town
+     * @return Location
+     */
+    public function setTown($town)
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    /**
+     * Get town
+     *
+     * @return string 
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    /**
+     * Set townReal
+     *
+     * @param string $townReal
+     * @return Location
+     */
+    public function setTownReal($townReal)
+    {
+        $this->townReal = $townReal;
+
+        return $this;
+    }
+
+    /**
+     * Get townReal
+     *
+     * @return string 
+     */
+    public function getTownReal()
+    {
+        return $this->townReal;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Location
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set access
+     *
+     * @param string $access
+     * @return Location
+     */
+    public function setAccess($access)
+    {
+        $this->access = $access;
+
+        return $this;
+    }
+
+    /**
+     * Get access
+     *
+     * @return string 
+     */
+    public function getAccess()
+    {
+        return $this->access;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Location
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set googlemap
+     *
+     * @param string $googlemap
+     * @return Location
+     */
+    public function setGooglemap($googlemap)
+    {
+        $this->googlemap = $googlemap;
+
+        return $this;
+    }
+
+    /**
+     * Get googlemap
+     *
+     * @return string 
+     */
+    public function getGooglemap()
+    {
+        return $this->googlemap;
+    }
+
+    /**
+     * Set live
+     *
+     * @param boolean $live
+     * @return Location
+     */
+    public function setLive($live)
+    {
+        $this->live = $live;
+
+        return $this;
+    }
+
+    /**
+     * Get live
+     *
+     * @return boolean 
+     */
+    public function getLive()
+    {
+        return $this->live;
+    }
+
+    /**
+     * Set deleted
+     *
+     * @param boolean $deleted
+     * @return Location
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted
+     *
+     * @return boolean 
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Location
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     * @return Location
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime 
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+}
