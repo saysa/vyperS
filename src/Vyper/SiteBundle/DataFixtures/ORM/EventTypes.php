8abd960040421f73e32a3cef903345a0f8c40085
saysa <saysabou@gmail.com>
2014-09-30 08:08:08 +0200

    EventType DataFixtures

---
+++ src/Vyper/SiteBundle/DataFixtures/ORM/EventTypes.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saysa
+ * Date: 09/09/2014
+ * Time: 07:15
+ */
+
+namespace Vyper\SiteBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Vyper\SiteBundle\Entity\EventType;
+
+
+class EventTypes implements FixtureInterface {
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $names = array(
+            'Concert',
+            'Event',
+        );
+
+        foreach ($names as $i => $name)
+        {
+            $list_continents[$i] = new EventType();
+            $list_continents[$i]->setName($name);
+
+            $manager->persist($list_continents[$i]);
+        }
+
+        $manager->flush();
+    }
+}
