5cccbb2ddd46089626ea02bcfd2fb247af9df94b
saysa <saysabou@gmail.com>
2014-10-04 11:48:02 +0200

    View:Index:index

--- src/Vyper/SiteBundle/Controller/IndexController.php
+++ src/Vyper/SiteBundle/Controller/IndexController.php
@@ -22,10 +22,20 @@ class IndexController extends Controller
 
         $type = $em->getRepository('VyperSiteBundle:ArticleType')->findBy(array('name' => 'News'));
         $latest_news = $em->getRepository('VyperSiteBundle:Article')->latestNews($type);
+        $type = $em->getRepository('VyperSiteBundle:ArticleType')->findBy(array('name' => 'Interview'));
+        $latest_interviews = $em->getRepository('VyperSiteBundle:Article')->latestNews($type);
+        $type = $em->getRepository('VyperSiteBundle:ArticleType')->findBy(array('name' => 'Live report'));
+        $latest_live_reports = $em->getRepository('VyperSiteBundle:Article')->latestNews($type);
+        $type = $em->getRepository('VyperSiteBundle:ArticleType')->findBy(array('name' => 'Chronique'));
+        $latest_chroniques = $em->getRepository('VyperSiteBundle:Article')->latestNews($type);
+
 
         $view
             ->set('articles_carousel', $articles_carousel)
             ->set('latest_news', $latest_news)
+            ->set('latest_interviews', $latest_interviews)
+            ->set('latest_live_reports', $latest_live_reports)
+            ->set('latest_chroniques', $latest_chroniques)
             ->set('front_page_index', true)
             ->set('user_id', $user)
         ;
