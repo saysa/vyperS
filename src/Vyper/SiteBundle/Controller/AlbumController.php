a7ea9244d4b26c8160e69cb77324f557f871fc96
Saysa Bounkhong <saysa@Saysa-3.local>
2014-10-07 11:05:58 +0200

    Album:viewAlbum

--- src/Vyper/SiteBundle/Controller/AlbumController.php
+++ src/Vyper/SiteBundle/Controller/AlbumController.php
@@ -3,7 +3,9 @@
 namespace Vyper\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Vyper\SiteBundle\Entity\Album;
 
 
 class AlbumController extends Controller
@@ -22,6 +24,21 @@ class AlbumController extends Controller
         return $this->render('VyperSiteBundle:Album:recentPictures.html.twig', $view->getView());
     }
 
+    public function showAlbumAction(Request $request, Album $album)
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        $increment = $this->container->get('vpr_visit_increment');
+        $increment->increment($album, $em);
+
+        $pictures  = $em->getRepository('VyperSiteBundle:Picture')->findBy(array('album' => $album->getId()));
+
+        $view = $this->container->get('saysa_view');
+        $view->set('pictures', $pictures);
+        $view->set('album', $album);
+        $view->set("img_type_news", "true");
+
+        return $this->render('VyperSiteBundle:Album:showAlbum.html.twig', $view->getView());
+    }
 
 }
